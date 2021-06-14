5eab673c1913a7d00b3d9e31ecd3af9074bfe288
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-06-14 15:59:59 +0200

    Commit

---
+++ Src/Shared/Tauron.Application.Dataflow/Internal/StartSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Tauron.Application.Dataflow.Internal
+{
+    internal sealed class StartSource
+    {
+        public static ISourceBlock<TOutput> Create<TOutput>(Func<TOutput> dataFunc)
+        {
+            var block = new WriteOnceBlock<TOutput>(d => d);
+            Task.Run(async () =>
+                     {
+                         await block.SendAsync(dataFunc());
+                     });
+            return block;
+        }
+
+        public static ISourceBlock<TOutput> Create<TOutput>(Func<Task<TOutput>> dataFunc)
+        {
+            var block = new WriteOnceBlock<TOutput>(d => d);
+            Task.Run(async () =>
+                     {
+                         await block.SendAsync(await dataFunc());
+                     });
+            return block;
+        }
+    }
+}
