6287004add8dd69d230046b67c9e31327099476d
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-07-09 07:21:34 +0200

    Commit

---
+++ Src/Apps/FrontEnd/ServiceManager/ServiceManager/Client/Components/Operations/IOperationManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServiceManager.Client.Components.Operations
+{
+    public interface IOperationManager
+    {
+        IDisposable Start();
+
+        IDisposable Subscribe(IObserver<bool> action);
+    }
+}
---
+++ Src/Apps/FrontEnd/ServiceManager/ServiceManager/Client/Components/Operations/OperationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace ServiceManager.Client.Components.Operations
+{
+    public sealed class OperationManager : IOperationManager, IDisposable
+    {
+        private readonly Subject<bool> _informer = new();
+
+        public IDisposable Start()
+        {
+            _informer.OnNext(true);
+            return Disposables.Create(() => _informer.OnNext(false));
+        }
+
+        public IDisposable Subscribe(IObserver<bool> action)
+            => _informer.Subscribe(action);
+
+        public void Dispose()
+        {
+            _informer.OnCompleted();
+            _informer.Dispose();
+        }
+    }
+}
