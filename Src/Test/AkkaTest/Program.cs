8a0781f6a497a2a82166c54d61095b4c40e50eb2
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-07-12 13:52:41 +0200

    Commit

--- Src/Test/AkkaTest/Program.cs
+++ Src/Test/AkkaTest/Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
+using Akka.Configuration;
+using Akka.Configuration.Hocon;
 using Tauron.Application.AkkaNode.Services.Core;
 using Tauron.Features;
 
@@ -11,8 +17,37 @@ namespace AkkaTest
     {
         private static async Task Main()
         {
-            var uri = new Uri("akka.tcp://Project-Manager@192.168.105.96:59169");
+            const string akka = "akka.conf";
+            string config = await File.ReadAllTextAsync(akka);
+            config = Extract(ConfigurationFactory.ParseString(config).Root.GetObject()).AggregateString((builder, s) => builder.AppendLine(s));
+
             Console.ReadKey();
         }
+
+        private static List<string> Extract(HoconObject config)
+        {
+            List<string> elements = new();
+
+            foreach (var (key, hoconValue) in config.Items.Where(hv => !hv.Value.IsEmpty))
+            {
+                var ele = key;
+                    if (hoconValue.IsString())
+                        elements.Add(ele + "=" + hoconValue.GetString());
+                    else if (hoconValue.IsArray())
+                        elements.Add(ele + "=" + hoconValue.GetArray().AggregateString((builder, value) => builder.Length == 0
+                                                                                           ? builder.Append("[ ").Append(value.GetString())
+                                                                                           : builder.Append(", ").Append(value.GetString())) + " ]");
+                    else if(hoconValue.IsObject())
+                        elements.AddRange(Extract(hoconValue.GetObject()).Select(s => ele + "." + s));
+                    else
+                        elements.Add(ele + "=" + hoconValue);
+
+            }
+
+            return elements;
+        }
+
+        private static string AggregateString<TType>(this IEnumerable<TType> enumerable, Func<StringBuilder, TType, StringBuilder> aggregator) 
+            => enumerable.Aggregate(new StringBuilder(), aggregator).ToString();
     }
 }
