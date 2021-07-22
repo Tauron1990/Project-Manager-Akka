702ff224c3974b122b2c12ef4b628cb2b708e876
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-07-22 22:13:29 +0200

    Commit

--- Src/Apps/FrontEnd/ServiceManager/ServiceManager/Client/ViewModels/ConfigurationViewAppConfigModel.cs
+++ Src/Apps/FrontEnd/ServiceManager/ServiceManager/Client/ViewModels/ConfigurationViewAppConfigModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ServiceHost.Client.Shared.ConfigurationServer.Data;
 using ServiceManager.Client.Components;
 using ServiceManager.Shared.ServiceDeamon;
 using Tauron.Application;
@@ -9,11 +11,25 @@ namespace ServiceManager.Client.ViewModels
     {
         private readonly IServerConfigurationApi _api;
         private bool _isEditing;
+        private bool _isLoading;
+        private IEnumerable<SpecificConfig> _appConfigs;
 
         public bool IsEditing
         {
             get => _isEditing;
-            set => SetProperty(ref _isEditing, value);
+            private set => SetProperty(ref _isEditing, value);
+        }
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            private set => SetProperty(ref _isLoading, value);
+        }
+
+        public IEnumerable<SpecificConfig> AppConfigs
+        {
+            get => _appConfigs;
+            private set => SetProperty(ref _appConfigs, value);
         }
 
         public ConfigurationViewAppConfigModel(IServerConfigurationApi api)
