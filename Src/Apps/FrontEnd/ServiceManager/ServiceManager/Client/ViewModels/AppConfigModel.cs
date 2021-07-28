31522623b861f2a1fe8a15853c6386833660298b
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-07-28 12:23:00 +0200

    Commit

--- Src/Apps/FrontEnd/ServiceManager/ServiceManager/Client/ViewModels/AppConfigModel.cs
+++ Src/Apps/FrontEnd/ServiceManager/ServiceManager/Client/ViewModels/AppConfigModel.cs
@@ -5,14 +5,8 @@ namespace ServiceManager.Client.ViewModels
 {
     public sealed class AppConfigModel : ObservableObject
     {
-        private bool _isEditing;
         public SpecificConfig Config { get; }
-
-        public bool IsEditing
-        {
-            get => _isEditing;
-            private set => SetProperty(ref _isEditing, value);
-        }
+        
 
         public AppConfigModel(SpecificConfig config) => Config = config;
     }
