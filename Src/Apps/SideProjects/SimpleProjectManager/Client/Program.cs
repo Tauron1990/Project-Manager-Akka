29ff1bb841cd71ec25ed6842539a1b4ede8cfc05
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-10-26 14:49:32 +0200

    Commit

---
+++ Src/Apps/SideProjects/SimpleProjectManager/Client/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using SimpleProjectManager.Client;
+using Stl.Fusion;
+using Stl.Fusion.Blazor;
+using Stl.Fusion.Client;
+using Stl.Fusion.Extensions;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigFusion(builder.Services, new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
+
+static void ConfigFusion(IServiceCollection collection, Uri baseAdress)
+{
+    collection.AddSingleton<BlazorModeHelper>();
+    collection.AddFusion()
+       .AddFusionTime()
+       .AddBlazorUIServices()
+       .AddAuthentication(
+            o => o.AddBlazor().AddRestEaseClient())
+       .AddRestEaseClient()
+       .ConfigureHttpClientFactory(
+            (_, _, options) => options.HttpClientActions.Add(
+                c =>
+                {
+                    Console.WriteLine($"Client Config: {c.BaseAddress} -- {baseAdress}");
+                    c.BaseAddress = baseAdress;
+                }))
+       .ConfigureWebSocketChannel(new WebSocketChannelProvider.Options { BaseUri = baseAdress });
+}
---
+++ Src/Apps/SideProjects/SimpleProjectManager/Server/Program.cs
@@ -0,0 +1,16 @@
+
+using SimpleProjectManager.Server;
+using Tauron.Application.AkkaNode.Bootstrap;
+using Tauron.Application.AkkaNode.Bootstrap.Console;
+using Tauron.Application.Master.Commands.KillSwitch;
+
+var builder = Host.CreateDefaultBuilder(args)
+   .StartNode(KillRecpientType.Frontend, IpcApplicationType.NoIpc, consoleLog: true)
+   .ConfigureWebHostDefaults(
+        b =>
+        {
+            b.UseUrls("http://localhost:85", "http://192.168.105.96:85");
+            b.UseStartup<Startup>();
+        });
+
+builder.Build().Run();
