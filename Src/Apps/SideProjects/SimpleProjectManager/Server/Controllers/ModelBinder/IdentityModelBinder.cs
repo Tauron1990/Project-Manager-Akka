7f1b8312e23a0ad3e07d6554901372f1600a0b93
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-11-05 15:23:59 +0100

    Commit

---
+++ Src/Apps/SideProjects/SimpleProjectManager/Server/Controllers/ModelBinder/IdentityModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using Akkatecture.Core;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Stl.Fusion.Server.Internal;
+using Tauron;
+
+namespace SimpleProjectManager.Server.Controllers.ModelBinder;
+
+public sealed class IdentityModelBinderFactory : IModelBinderProvider
+{
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+        var model = context.Metadata.ModelType;
+
+        if (model.IsAssignableTo(typeof(IIdentity)) && model.GetConstructor(new[] { typeof(string) }) != null)
+            return new IdentityModelBinder();
+
+        return null;
+    }
+}
+
+public sealed class IdentityModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        try
+        {
+            var id = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).FirstValue ?? string.Empty;
+            bindingContext.Result = ModelBindingResult.Success(FastReflection.Shared.FastCreateInstance(bindingContext.ModelType, id));
+        }
+        catch (Exception)
+        {
+            bindingContext.Result = ModelBindingResult.Failed();
+        }
+
+        return Task.CompletedTask;
+    }
+}
