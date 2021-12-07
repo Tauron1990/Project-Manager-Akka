ab77de957b3cc8f7d0b9e401f25bdba7fa128edf
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-12-07 19:24:27 +0100

    Commit

--- Src/Apps/SideProjects/SimpleProjectManager/Shared/OperationClient/NameFeature.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/Shared/OperationClient/NameFeature.cs
@@ -1,6 +1,18 @@
-﻿namespace SimpleProjectManager.Shared.OperationClient;
+﻿using System.Reactive.Linq;
+using Tauron;
+using Tauron.Features;
 
-public class NameFeature
+namespace SimpleProjectManager.Shared.OperationClient;
+
+public sealed class NameFeature : ActorFeatureBase<NameFeature.State>
 {
+    public sealed record State(string Name);
+
+    public static IPreparedFeature Create(string name)
+        => Feature.Create(() => new NameFeature(), _ => new State(name));
     
+    protected override void ConfigImpl()
+    {
+        Receive<NameRequest>(o => o.Select(s => s.NewEvent(new NameResponse(s.State.Name))).ToSender());
+    }
 }
--- Src/Apps/SideProjects/SimpleProjectManager/Shared/OperationClient/NameRequest.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/Shared/OperationClient/NameRequest.cs
@@ -3,13 +3,17 @@ using Microsoft.Extensions.Logging;
 
 namespace SimpleProjectManager.Shared.OperationClient;
 
-public sealed record NameRequest(string Name)
+public sealed record NameResponse(string Name);
+
+public sealed record NameRequest
 {
     public async Task<string?> Ask(IActorRef actorRef, ILogger logger)
     {
         try
         {
-            
+            var response = await actorRef.Ask<NameResponse>(new NameRequest(), TimeSpan.FromSeconds(10));
+
+            return response.Name;
         }
         catch (Exception e)
         {
