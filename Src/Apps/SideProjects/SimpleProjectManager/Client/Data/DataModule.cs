9a5a74bbf19a501adef30ea51d44c60aeacb4750
Alexander Bachmaier <Tauron.ab@gmail.com>
2021-12-30 05:46:27 +0100

    Commit

--- Src/Apps/SideProjects/SimpleProjectManager/Client/Data/DataModule.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/Client/Data/DataModule.cs
@@ -1,7 +1,5 @@
 ﻿using SimpleProjectManager.Client.Data.Cache;
-using Stl.Fusion.Swapping;
 using Tauron;
-using Tauron.Application.Workshop;
 using Tavenem.Blazor.IndexedDB;
 
 namespace SimpleProjectManager.Client.Data;
@@ -10,11 +8,12 @@ public class DataModule : IModule
 {
     public void Load(IServiceCollection collection)
     {
-        collection.RegisterModules(new CommonModule(), new DataModule(), new TaskModule());
+        collection.RegisterModules(new CommonModule(), new DataModule());
         
         collection.AddIndexedDb(new IndexedDb<string>(nameof(CacheData)));
         collection.AddIndexedDb(new IndexedDb<int>(nameof(CacheTimeout)));
+        
         collection.AddScoped<CacheDb>();
-        collection.AddScoped<TimeoutManager>();
+        collection.AddSingleton<TimeoutManager>();
     }
 }
