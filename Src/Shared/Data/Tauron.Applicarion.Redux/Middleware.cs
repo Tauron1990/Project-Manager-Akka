ac0759ed24401c9d37e281afcdfc36de531a9abd
Alexander Bachmaier <Tauron.ab@gmail.com>
2022-01-04 20:24:43 +0100

    Commit

--- Src/Shared/Data/Tauron.Applicarion.Redux/Middleware.cs
+++ Src/Shared/Data/Tauron.Applicarion.Redux/Middleware.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Reactive.Linq;
+using JetBrains.Annotations;
 
 namespace Tauron.Applicarion.Redux;
 
@@ -7,17 +8,22 @@ public abstract class Middleware<TState> : IMiddleware<TState>
 {
     protected sealed record TypedDispatchedAction<TAction>(TState State, TAction Action);
     
-    private List<TypedDispatchedAction<TState>>? _processors;
+    private List<TypedDispatchedAction<TState>> _processors = new();
 
     protected IStore<TState> Store { get; private set; } = null!;
-
-    
     
     public virtual void Initialize(IStore<TState> store)
         => Store = store;
 
     public IObservable<TState> Connect(IObservable<DispatchedAction<TState>> action, DispatchNext<TState> next)
     {
+        Initialize();
         
+        return action.SelectMany()
     }
+
+    protected void Dispatch(object action)
+        => Store.Dispatch(action);
+
+    protected abstract void Initialize();
 }
