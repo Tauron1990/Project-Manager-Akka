f43ec217895802ae5de05a51cdd287e7c480afe2
Alexander Bachmaier <Tauron.ab@gmail.com>
2022-02-18 08:31:46 +0100

    Commit

--- Src/Apps/SideProjects/SimpleProjectManager/Client/ClientDev/src/js/Database/DatabaseContext.js
+++ Src/Apps/SideProjects/SimpleProjectManager/Client/ClientDev/src/js/Database/DatabaseContext.js
@@ -45,8 +45,13 @@ var DatabaseContext;
             this.id = id;
             this.data = data;
         }
-        DataContainer.defaultStoreName = "store";
         return DataContainer;
+    }());
+    var StaticData = /** @class */ (function () {
+        function StaticData() {
+        }
+        StaticData.database = null;
+        return StaticData;
     }());
     var Result = /** @class */ (function () {
         function Result(msg) {
@@ -63,22 +68,100 @@ var DatabaseContext;
     DatabaseContext.Result = Result;
     function openDataDatabase() {
         return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, (0, idb_1.openDB)("StateData", 1, {
-                            upgrade: function (db) {
-                                db.createObjectStore("timeout", { keyPath: "id" });
-                                db.createObjectStore("data", { keyPath: "id" });
-                            }
-                        })];
-                    case 1: return [2 /*return*/, _a.sent()];
+            var _a;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        if (!(StaticData.database == null)) return [3 /*break*/, 2];
+                        _a = StaticData;
+                        return [4 /*yield*/, (0, idb_1.openDB)("StateData", 1, {
+                                upgrade: function (db) {
+                                    db.createObjectStore("timeout", { keyPath: "id" });
+                                    db.createObjectStore("data", { keyPath: "id" });
+                                }
+                            })];
+                    case 1:
+                        _a.database = _b.sent();
+                        _b.label = 2;
+                    case 2: return [2 /*return*/, StaticData.database];
                 }
             });
         });
     }
-    function saveData(id, data) {
+    function getAllTimeoutElements() {
+        return __awaiter(this, void 0, void 0, function () {
+            var db, dataList;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, openDataDatabase()];
+                    case 1:
+                        db = _a.sent();
+                        return [4 /*yield*/, db.getAll("timeout")];
+                    case 2:
+                        dataList = _a.sent();
+                        return [2 /*return*/, dataList.map(function (v) { return v.data; })];
+                }
+            });
+        });
+    }
+    DatabaseContext.getAllTimeoutElements = getAllTimeoutElements;
+    function deleteElement(id, timeoutId) {
+        return __awaiter(this, void 0, void 0, function () {
+            var db, transaction, timeoutStore, dataStore, timeoutData;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, openDataDatabase()];
+                    case 1:
+                        db = _a.sent();
+                        transaction = db.transaction(["timeout", "data"], "readwrite");
+                        timeoutStore = transaction.objectStore(transaction.objectStoreNames[0]);
+                        dataStore = transaction.objectStore(transaction.objectStoreNames[1]);
+                        return [4 /*yield*/, timeoutStore.get(timeoutId)];
+                    case 2:
+                        timeoutData = _a.sent();
+                        if (!(timeoutData != undefined)) return [3 /*break*/, 4];
+                        return [4 /*yield*/, timeoutStore["delete"](timeoutId)];
+                    case 3:
+                        _a.sent();
+                        _a.label = 4;
+                    case 4: return [4 /*yield*/, dataStore["delete"](id)];
+                    case 5:
+                        _a.sent();
+                        return [4 /*yield*/, transaction.done];
+                    case 6:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    }
+    DatabaseContext.deleteElement = deleteElement;
+    function deleteTimeoutElement(id) {
         return __awaiter(this, void 0, void 0, function () {
             var db, e_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 3, , 4]);
+                        return [4 /*yield*/, openDataDatabase()];
+                    case 1:
+                        db = _a.sent();
+                        return [4 /*yield*/, db["delete"]("timeout", id)];
+                    case 2:
+                        _a.sent();
+                        return [2 /*return*/, new Result()];
+                    case 3:
+                        e_1 = _a.sent();
+                        return [2 /*return*/, new Result(e_1.toString())];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    }
+    DatabaseContext.deleteTimeoutElement = deleteTimeoutElement;
+    function saveData(id, data) {
+        return __awaiter(this, void 0, void 0, function () {
+            var db, e_2;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
@@ -91,8 +174,8 @@ var DatabaseContext;
                         _a.sent();
                         return [2 /*return*/, new Result()];
                     case 3:
-                        e_1 = _a.sent();
-                        return [2 /*return*/, new Result(e_1.toString())];
+                        e_2 = _a.sent();
+                        return [2 /*return*/, new Result(e_2.toString())];
                     case 4: return [2 /*return*/];
                 }
             });
--- Src/Apps/SideProjects/SimpleProjectManagerV2/SimpleProjectManagerV2/Program.cs
+++ Src/Apps/SideProjects/SimpleProjectManagerV2/SimpleProjectManagerV2/Program.cs
@@ -6,9 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSpaStaticFiles();
+
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+#if DEBUG
+app.UseDeveloperExceptionPage();
+#endif
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
