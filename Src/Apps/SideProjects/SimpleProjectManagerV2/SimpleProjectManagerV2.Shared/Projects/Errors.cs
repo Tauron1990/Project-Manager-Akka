6e4cdf1caff2e428cf03227778e99d75fc75c809
Alexander Bachmaier <Tauron.ab@gmail.com>
2022-02-18 04:58:53 +0100

    Commot

---
+++ Src/Apps/SideProjects/SimpleProjectManagerV2/SimpleProjectManagerV2.Shared/Projects/Errors.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleProjectManagerV2.Shared.Projects;
+
+public static class Errors
+{
+    public const string NoNewError = "no-new-error";
+    public const string NewError = "new-Error";
+}
