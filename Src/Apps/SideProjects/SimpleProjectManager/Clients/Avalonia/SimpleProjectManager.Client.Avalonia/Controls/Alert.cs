4ef1d802ed921692d0b85ebc6af54cc34b3e8cf2
Alexander Bachmaier <Tauron.ab@gmail.com>
2022-03-28 14:16:53 +0200

    commit

---
+++ Src/Apps/SideProjects/SimpleProjectManager/Clients/Avalonia/SimpleProjectManager.Client.Avalonia/Controls/Alert.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Controls;
+
+namespace SimpleProjectManager.Client.Avalonia.Controls;
+
+public sealed partial class Alert : UserControl
+{
+    public Alert()
+        => InitializeComponent();
+}
--- Src/Apps/SideProjects/SimpleProjectManager/Clients/Avalonia/SimpleProjectManager.Client.Avalonia/ViewModels/MainWindowViewModel.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/Clients/Avalonia/SimpleProjectManager.Client.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
+using Material.Styles;
+using ReactiveUI;
+using SimpleProjectManager.Client.Avalonia.Models;
 using SimpleProjectManager.Client.Shared.ViewModels;
+using Tauron.Application;
 
 namespace SimpleProjectManager.Client.Avalonia.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            
+        }
+        []
+        public MainWindowViewModel(IEventAggregator aggregator)
+        {
+            this.WhenActivated(
+                dispo =>
+                {
+                    aggregator.ConsumeSharedMessage()
+                       .ObserveOn(RxApp.MainThreadScheduler)
+                       .Subscribe(m => SnackbarHost.Post("Test"))
+                       .DisposeWith(dispo);
+                });
+        }
     }
 }
