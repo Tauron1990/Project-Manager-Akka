eeefc3a5f3b25c4dc93070132db1fd079491571f
Alexander <Tauron.ab@gmail.com>
2022-08-23 13:37:37 +0200

    Commit

--- Src/Apps/SideProjects/SimpleProjectManager/Data/SimpleProjectManager.Server.Configuration/StartConfigManager.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/Data/SimpleProjectManager.Server.Configuration/StartConfigManager.cs
@@ -1,6 +1,14 @@
-﻿namespace SimpleProjectManager.Server.Configuration;
+﻿using System.Collections.Immutable;
+using SimpleProjectManager.Server.Configuration.ConfigurationExtensions;
+
+namespace SimpleProjectManager.Server.Configuration;
 
 public sealed class StartConfigManager
 {
+    private ImmutableArray<IConfigExtension> _extensions = ImmutableArray<IConfigExtension>.Empty;
+
     public static StartConfigManager ConfigManager { get; } = new();
+
+    public void RegisterExtension(IConfigExtension extension)
+        => _extensions = _extensions.Add(extension);
 }
