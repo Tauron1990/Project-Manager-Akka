124678f5a04854460b38311906fd5bca8710ba8c
Alexander <Tauron.ab@gmail.com>
2023-03-02 14:02:03 +0100

    Commit

--- Src/Test/TestApp/Program.cs
+++ Src/Test/TestApp/Program.cs
@@ -1,4 +1,16 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using BenchmarkDotNet.Running;
+using LanguageExt;
+using LanguageExt.Pipes;
+
+using LanguageExt;
+using static LanguageExt.Prelude;
+using static LanguageExt.List;
+using LanguageExt.ClassInstances;
+using LanguageExt.ClassInstances.Const;
+using LanguageExt.ClassInstances.Pred;
+using LanguageExt.SomeHelp;
+using LanguageExt.TypeClasses;
 
 namespace TestApp;
 
@@ -8,7 +20,18 @@ internal class Program
 {
     private static void Main()
     {
-        BenchmarkRunner.Run<Benchmarks>();
+        //BenchmarkRunner.Run<Benchmarks>();
+        
+        var result =
+            from initial in Some(10)
+            from add1 in Add(initial, 10)
+            let add2 = add1 + initial
+            from add3 in Add(add2, 10)
+            select add3 + add2 + add1 + initial;
+
+        result.Iter(Console.WriteLine);
     }
 
+    public static Option<int> Add(int value, int value2)
+        => Some(value + value2);
 }
