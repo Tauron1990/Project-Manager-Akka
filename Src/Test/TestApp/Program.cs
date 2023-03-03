594ea3ef6122ed782e8bb77fca10136bcf3e3421
Alexander <Tauron.ab@gmail.com>
2023-03-03 11:03:47 +0100

    Commit

--- Src/Test/TestApp/Program.cs
+++ Src/Test/TestApp/Program.cs
@@ -1,8 +1,14 @@
-﻿using LanguageExt;
+﻿
+
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageExt;
 using LanguageExt.Pipes;
 using LanguageExt.Sys;
 using LanguageExt.Sys.IO;
-using LanguageExt.Sys.Live;
+using LanguageExt.SysX.Live;
 
 namespace TestApp;
 
@@ -10,20 +16,42 @@ namespace TestApp;
 
 internal class Program
 {
-    private static void Main()
+    private static async  Task Main()
     {
         //BenchmarkRunner.Run<Benchmarks>();
 
         var open = File<Runtime>.openRead("Test.txt");
-        var pipe = Stream<Runtime>.read(1000);
+        
+        var reader = Stream<Runtime>.read(1000);
+        
+        var decoder =
+            from data in Proxy.awaiting<SeqLoan<byte>>()
+            from _ in Proxy.yield(Encoding.UTF8.GetString(data.ToReadOnlySpan()))
+            select Unit.Default;
 
         var consumer =
-            from data in Consumer.awaiting<Runtime, SeqLoan<byte>>()
-            from ebc in Enc<Runtime>.encoding
-            select ebc.GetString(data.ToSpan());
+            from data in Proxy.awaiting<string>()
+            from _ in Console<Runtime>.writeLine(data)
+            select Unit.Default;
+            
+            
+
+        var test = open | reader | decoder.Interpret<Runtime>() | consumer;
+
+        var main =
+            from _ in test
+            from res in Console<Runtime>.readKey
+            select res;
 
 
-        var combine = open | pipe;
+        var fin = await main.Run(Runtime.New());
 
+        fin.ThrowIfFail();
+
+
+        // var consumer =
+        //     from data in Consumer.awaiting<Runtime, SeqLoan<byte>>()
+        //     from ebc in Enc<Runtime>.encoding
+        //     select ebc.GetString(data.ToSpan());
     }
 }
