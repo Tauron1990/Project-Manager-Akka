5b5f8b6c6ee755e9dfe928d9dc8c3b97d69af8f9
Alexander <Tauron.ab@gmail.com>
2023-04-13 10:45:24 +0200

    Commit

--- Src/Apps/SideProjects/SimpleProjectManager/SimpleProjectManager.Operation.Client/Device/MGI/Logging/SingleClientManager.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/SimpleProjectManager.Operation.Client/Device/MGI/Logging/SingleClientManager.cs
@@ -2,7 +2,6 @@
 using System.Threading.Channels;
 using Akka.Actor;
 using Microsoft.Extensions.Logging;
-using Tauron.Application;
 using Tauron.Features;
 using Tauron.Servicemnager.Networking.Data;
 
@@ -10,30 +9,38 @@ namespace SimpleProjectManager.Operation.Client.Device.MGI.Logging;
 
 public sealed partial class SingleClientManager : ActorFeatureBase<SingleClientManager.State>
 {
-    public sealed record State(IMessageStream Client, ChannelWriter<LogInfo> Writer, LogParser LogParser, string App = "Unkowen");
+    public sealed record State(IMessageStream Client, ChannelWriter<LogInfo> Writer, LogParser LogParser, string App = "Unkowen") : IDisposable
+    {
+        public void Deconstruct(out ChannelWriter<LogInfo> writer, out LogParser logparser)
+        {
+            writer = Writer;
+            logparser = LogParser;
+        }
 
+        public void Dispose()
+        {
+            Client.Dispose();
+            LogParser.Dispose();
+        }
+    }
+
+    private sealed record NewName(string Name);
+    
     private readonly ILogger _logger;
 
-    public static IPreparedFeature New
+    public static IPreparedFeature New(IMessageStream client, ChannelWriter<LogInfo> writer)
+        => Feature.Create(() => new SingleClientManager(), _ => new State(client, writer, new LogParser(client)));
 
-    private SingleClientManager(IMessageStream client, ChannelWriter<LogInfo> writer)
-    {
-        _logger = Logger;
-        
-        _client = client;
-        _writer = writer;
-        _logParser = new LogParser(client);
-        
-        Receive<ClientError>(OnFailure);
-        
-        Run(Self).PipeTo(Self,
-            success:() => PoisonPill.Instance,
-            failure: e => ClientError.CreateError("Error on Process Socket Receive Data", e));
-    }
+    private SingleClientManager() => _logger = Logger;
 
     protected override void ConfigImpl()
     {
+        Receive<ClientError>(OnFailure);
+        ReceiveState<NewName>(p => p.State with { App = p.Event.Name });
         
+        Run(CurrentState, Self).PipeTo(Self,
+            success:() => PoisonPill.Instance,
+            failure: e => ClientError.CreateError("Error on Process Socket Receive Data", e));
     }
 
     private void OnFailure(ClientError obj)
@@ -46,12 +53,14 @@ public sealed partial class SingleClientManager : ActorFeatureBase<SingleClientM
     private partial void ReceiveError(Exception? ex, string message);
 
     [SuppressMessage("ReSharper", "MethodSupportsCancellation")]
-    private async Task Run(IActorRef self)
+    private static async Task Run(State state, IActorRef self)
     {
-        using var source = new CancellationTokenSource(); 
+        using var source = new CancellationTokenSource();
+
+        (var writer, LogParser logParser) = state;
         
         var logs = Channel.CreateBounded<LogInfo>(1000);
-        Task runner = _logParser.Run(logs.Writer, source.Token);
+        Task runner = logParser.Run(logs.Writer, source.Token);
 
         try
         {
@@ -60,22 +69,22 @@ public sealed partial class SingleClientManager : ActorFeatureBase<SingleClientM
                 LogInfo info = infoItem;
 
                 if(string.IsNullOrWhiteSpace(info.Application))
-                    info = info with { Application = _app };
+                    info = info with { Application = state.App };
 
                 // ReSharper disable once SwitchStatementMissingSomeEnumCasesNoDefault
                 switch (info.Command)
                 {
                     case Command.Disconnect:
-                        await _writer.WriteAsync(info).ConfigureAwait(false);
+                        await writer.WriteAsync(info, source.Token).ConfigureAwait(false);
 
                         return;
                     case Command.SetApp:
-                        _app = info.Content;
-
+                        state = state with { App = info.Content };
+                        self.Tell(new NewName(info.Content));
                         break;
                 }
 
-                await _writer.WriteAsync(info).ConfigureAwait(false);
+                await writer.WriteAsync(info, source.Token).ConfigureAwait(false);
             }
         }
         catch (ObjectDisposedException) { }
@@ -85,21 +94,15 @@ public sealed partial class SingleClientManager : ActorFeatureBase<SingleClientM
         }
         finally
         {
-            _writer.TryComplete();
+            writer.TryComplete();
             source.Cancel();
         }
         
         await runner.ConfigureAwait(false);
     }
     
-    protected override void PostStop()
-    {
-        _logParser.Dispose();
-        base.PostStop();
-    }
-
     public void Dispose()
-        => _client.Dispose();
+        => CurrentState.Dispose();
 
 
     private record ClientError(Exception? Error, string Message)
