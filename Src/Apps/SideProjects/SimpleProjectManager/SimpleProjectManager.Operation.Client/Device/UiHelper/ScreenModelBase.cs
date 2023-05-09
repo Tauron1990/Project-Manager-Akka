9ca0c0ee0755e5ce37b3a9a0a767afc9269173e7
Alexander <Tauron.ab@gmail.com>
2023-05-09 12:11:23 +0200

    Commit

--- Src/Apps/SideProjects/SimpleProjectManager/SimpleProjectManager.Operation.Client/Device/UiHelper/ScreenModelBase.cs
+++ Src/Apps/SideProjects/SimpleProjectManager/SimpleProjectManager.Operation.Client/Device/UiHelper/ScreenModelBase.cs
@@ -1,21 +1,29 @@
-﻿using System.Reactive.Subjects;
+﻿using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
 using SimpleProjectManager.Client.Operations.Shared.Devices;
 using SimpleProjectManager.Shared.Services.Devices;
-using Stl.Fusion;
+using Tauron;
 using Tauron.Operations;
 
 namespace SimpleProjectManager.Operation.Client.Device.UiHelper;
 
-public abstract class ScreenModelBase : IScreenModel
+[PublicAPI]
+public abstract partial class ScreenModelBase : IScreenModel
 {
     private static readonly DeviceUiGroup Empty = DeviceUi.Text("Empty");
 
-    private readonly Dictionary<DeviceId, Func<SimpleResult, string>> _inputHandlers = new();
+    private readonly Dictionary<DeviceId, Func<string, SimpleResult>> _inputHandlers = new();
     private readonly Dictionary<DeviceId, Func<DeviceSensor, Task<DeviceManagerMessages.ISensorBox>>> _sensorHandlers = new();
     private readonly Dictionary<DeviceId, ButtonRegistraction> _buttons = new();
 
+    private readonly ILogger _logger;
+    
     private DeviceUiGroup _current = Empty;
     private Action<DeviceUiGroup> _updateUi = static _ => { };
+
+    protected ScreenModelBase(ILogger logger) => _logger = logger;
 
     public DeviceUiGroup Initialize()
     {
@@ -33,23 +41,41 @@ public abstract class ScreenModelBase : IScreenModel
 
     public virtual void OnHide() => _updateUi = static _ => { };
 
-    public virtual Task<DeviceManagerMessages.ISensorBox> UpdateSensorValue(DeviceSensor sensor) =>
-        Task.FromResult(DeviceManagerMessages.SensorBox.CreateDefault(sensor.SensorType));
+    [LoggerMessage(1, LogLevel.Warning, "No Sensor Registraion Found for {sensor}")]
+    private partial void SensorNotFound(DeviceSensor sensor);
+    
+    public virtual Task<DeviceManagerMessages.ISensorBox> UpdateSensorValue(DeviceSensor sensor) => 
+        _sensorHandlers.TryGetValue(sensor.Identifer, out var handller) 
+            ? handller(sensor) 
+            : Task.FromResult(DeviceManagerMessages.SensorBox.CreateDefault(sensor.SensorType));
 
-    public virtual void ButtonClick(DeviceId identifer) { }
+    public virtual void ButtonClick(DeviceId identifer)
+    {
+        
+    }
 
     public virtual void WhenButtonStateChanged(DeviceId identifer, Action<bool> onButtonStateChanged) { }
 
     public virtual Task<SimpleResult> NewInput(DeviceId element, string input) =>
         Task.FromResult(SimpleResult.Failure("NewInput not Implemented"));
 
-    public void UpdateUi(Func<DeviceUiGroup, DeviceUiGroup> updater)
+    protected void UpdateUi(Func<DeviceUiGroup, DeviceUiGroup> updater)
     {
         _current = updater(_current);
         _updateUi(_current);
     }
 
+    protected void HandleInput(DeviceId id, Func<string, SimpleResult> handler) =>
+        _inputHandlers.Add(id, handler);
+
+    protected void NewSensor(DeviceId id, Func<DeviceSensor, Task<DeviceManagerMessages.ISensorBox>> handler) =>
+        _sensorHandlers.Add(id, handler);
     
-    
-    private sealed record ButtonRegistraction(BehaviorSubject<bool> CanClick, Action OnClick, IDisposable Subscription);
+    protected void HandleButton(DeviceId id, IObservable<bool> canClick, Action onClick)
+    {
+        var canclickBehavior = new BehaviorSubject<bool>(value: false);
+        _buttons.Add(id, new ButtonRegistraction(canclickBehavior, onClick, canClick.AutoSubscribe(canclickBehavior), new SerialDisposable()));
+    }
+
+    private sealed record ButtonRegistraction(BehaviorSubject<bool> CanClick, Action OnClick, IDisposable Subscription, SerialDisposable ExtetnalSub);
 }
